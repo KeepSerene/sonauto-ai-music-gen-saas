e05c404fb8cfaf73f49087297651990f14348a94
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2026-04-20 06:37:11 +0530

    minor tweaks

--- web-app/src/components/audio-player/LyricsModal.tsx
+++ web-app/src/components/audio-player/LyricsModal.tsx
@@ -68,7 +68,7 @@ export default function LyricsModal({
 
           <Button
             type="button"
-            variant="secondary"
+            variant="outline"
             size="sm"
             onClick={handleCopyLyrics}
           >
